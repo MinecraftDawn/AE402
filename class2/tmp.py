9d120d85d526405f895ae6d78e2bab4648b2d9c0
MinecraftDawn <eric4902077@gmail.com>
2020-03-21 15:23:07 +0800

    第三堂課

--- class2/tmp.py
+++ class2/tmp.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 14 15:05:09 2020
+Created on Sat Mar 21 13:35:49 2020
 
 @author: eric
 """
+import time
 
-myList = [[1,2], [3,4]]
+# 遞迴 (Recursion)
+"""
+1. 必須要有終止的條件
+2. 必須要趨近於終止的條件
+3. 遞迴很慢 (比迭代慢 for迴圈)
+"""
+#def recursive(count):
+#    if count > 0:
+#        print(count)
+#        recursive(count-1)
+#
+#recursive(5)
 
-w = myList[0][1]
-w += 100
+a = 10
 
-print(myList)
+if a < 20:
+    print("<20")
+if a < 15:
+    print("<15")
+
